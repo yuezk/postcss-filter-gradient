ac4bb2e01eef6b99099f9397f07d3412f778fbdc
Kevin Yue <yuezk001@gmail.com>
2015-11-13 13:30:04 +0800

    Almost done, will add angle gradient support later

--- index.js
+++ index.js
@@ -1,12 +1,116 @@
 var postcss = require('postcss');
+var list = postcss.list;
+var filterGradient = require('filter-gradient');
+var gradientParser = require('gradient-parser');
+var DECL_FILTER = /^background(-image)?$/;
+
+// var ANGLE_MAP = {
+//     'top': 0,
+//     'right': 90,
+//     'bottom': 180,
+//     'left': 270,
+//     'top left': 315,
+//     'top right': 45,
+//     'bottom left': 225,
+//     'bottom right': 135
+// };
+
+function hasFilter(rule) {
+    var has = false;
+    rule.walkDecls(/(-ms-)?filter/, function () {
+        has = true;
+    });
+    return has;
+}
+
+function getGradientsFromDecl(decl) {
+    return list.comma(decl.value).filter(function (seg) {
+        return seg.indexOf('linear-gradient') === 0;
+    });
+}
+
+function getGradientFromRule(rule) {
+    var gradient = {};
+    rule.walkDecls(DECL_FILTER, function (decl) {
+        var gradients =  getGradientsFromDecl(decl);
+        if (gradients.length) {
+            gradient.value = gradients[0];
+            gradient.decl = decl;
+        }
+    });
+
+    return gradient;
+}
+
+function getColorStr(rawColor) {
+    var colorStr;
+
+    switch (rawColor.type) {
+    case 'hex':
+        colorStr = '#' + rawColor.value;
+        break;
+    case 'rgb':
+        colorStr = 'rgb(' + rawColor.value.join(',') + ')';
+        break;
+    case 'rgba':
+        colorStr = 'rgba(' + rawColor.value.join(',') + ')';
+        break;
+    case 'literal':
+        colorStr = rawColor.value;
+        break;
+    default:
+        colorStr = 'transparent';
+        break;
+    }
+
+    return colorStr;
+}
+
+function getGradientDirection(orientation) {
+    if (orientation === undefined) {
+        return 'bottom';
+    } else if (orientation.type === 'angular') {
+        console.log(orientation.value);
+    } else {
+        return orientation.value;
+    }
+}
+
+function getFilterFromGradient(gradient) {
+    var obj = gradientParser.parse(gradient)[0];
+    var startColor = getColorStr(obj.colorStops[0]);
+    var endColor = getColorStr(obj.colorStops[obj.colorStops.length - 1]);
+    var direction = getGradientDirection(obj.orientation);
+    var type;
+    var tmp;
+
+    // Swap color if needed;
+    if (/top|left/.test(direction)) {
+        tmp = startColor;
+        startColor = endColor;
+        endColor = tmp;
+    }
+    // 0: vertical, 1:horizontal
+    type = /top|bottom/.test(direction) ? 0 : 1;
+
+    return filterGradient(startColor, endColor, type);
+}
 
 module.exports = postcss.plugin('postcss-filter-gradient', function (opts) {
     opts = opts || {};
-    var filter = /^background(-image)?$/;
 
-    return function (css) {
-        css.walkDecls(filter, function (decl) {
+    return function (root) {
+        root.walkRules(function (rule) {
+            var gradient;
+            var filter;
 
+            if (!hasFilter(rule)) {
+                gradient = getGradientFromRule(rule);
+                if (gradient.value) {
+                    filter = getFilterFromGradient(gradient.value);
+                    gradient.decl.cloneAfter({ prop: 'filter', value: filter });
+                }
+            }
         });
     };
 });
--- test/test.js
+++ test/test.js
@@ -1,10 +1,16 @@
+var fs = require('fs');
+var path = require('path');
 var postcss = require('postcss');
 var expect  = require('chai').expect;
 
 var plugin = require('../');
 
-var test = function (input, output, opts, done) {
-    postcss([ plugin(opts) ]).process(input).then(function (result) {
+var test = function (inputFile, opts, done) {
+    var prefix = path.join(__dirname, '/fixtures/', inputFile);
+    var input = fs.readFileSync(prefix + '.css', 'utf-8');
+    var output = fs.readFileSync(prefix + '.expect.css', 'utf-8');
+
+    postcss([plugin(opts)]).process(input).then(function (result) {
         expect(result.css).to.eql(output);
         expect(result.warnings()).to.be.empty;
         done();
@@ -14,11 +20,47 @@ var test = function (input, output, opts, done) {
 };
 
 describe('postcss-filter-gradient', function () {
+    it('should do nothing if linear-gradient not exists', function (done) {
+        test('none', {}, done);
+    });
 
-    /* Write tests here
+    it('should ignore the old syntax', function (done) {
+        test('old', {}, done);
+    });
 
-    it('does something', function (done) {
-        test('a{ }', 'a{ }', { }, done);
-    });*/
+    it('should do nothing if the filter/-ms-filter present', function (done) {
+        test('filter', {}, done);
+    });
 
+    it('should support the standard syntax', function (done) {
+        test('standard', {}, done);
+    });
+
+    it('should support syntax which omit direction', function (done) {
+        test('simple', {}, done);
+    });
+
+    it('should support vertical reverse', function (done) {
+        test('vertical-reverse', {}, done);
+    });
+
+    it('should support horizontal direction', function (done) {
+        test('horizontal', {}, done);
+    });
+
+    it('should support horizontal reverse', function (done) {
+        test('horizontal-reverse', {}, done);
+    });
+
+    it('should support multi color stops', function (done) {
+        test('multi-colorstops', {}, done);
+    });
+
+    it('should support multi background', function (done) {
+        test('multi-background', {}, done);
+    });
+
+    it.skip('should support angular gradient', function (done) {
+        test('angular', {}, done);
+    });
 });
